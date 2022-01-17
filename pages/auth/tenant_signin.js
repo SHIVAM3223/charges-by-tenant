840f90af6f56e71d1872daf56729704b1d0ddb82
SHIVAM3223 <shivambhushan653@gmail.com>
2022-01-18 00:18:40 +0530

    Update tenant_signin.js

--- pages/auth/tenant_signin.js
+++ pages/auth/tenant_signin.js
@@ -2,8 +2,77 @@ import Image from "next/image";
 import Tenant from "../../public/images/tenant.png";
 import Home_fill from "../../public/images/Home_fill.png";
 import Ellipse47 from "../../public/images/Ellipse47.png";
+<<<<<<< Updated upstream
+=======
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { route } from "next/dist/server/router";
+>>>>>>> Stashed changes
 
 function tenant_signin() {
+  const router = useRouter();
+
+  const [state, setState] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  /* 	"email":"rishabhborn2win@gmail.com",
+  "password": "Rishu@123" */
+
+  // function getPath() {
+  //   return "/landing/tenant";
+  // }
+
+  async function tenantSignIn() {
+    console.log("success");
+  }
+
+  async function signIn() {
+    let email = state.username;
+    let password = state.password;
+
+    let item = { email, password };
+
+    let result = await fetch("http://localhost:3000/api/auth/users/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(item),
+    });
+
+    console.log(item);
+    result = await result.json();
+    console.warn("result: " + JSON.stringify(result));
+
+    if (result.sucess == true) {
+      console.log("success");
+      router.push("/landing/tenant");
+    }
+    // result.sucess == true ? router.push("/landing/tenant") : alert("Enter correct credential");
+
+  }
+
+  function yourInput() {
+    if (state.username.length == 0) {
+      alert("Enter Username");
+    } else if (state.password.length == 0) {
+      alert("Enter Password");
+    } else {
+      alert("Username: " + state.username + "\nPassword: " + state.password);
+      signIn();
+    }
+  }
+
   return (
     <>
       <div className="main1">
@@ -33,12 +102,39 @@ function tenant_signin() {
                 <h2 className="form-title pr_form-title">Tenant Sign In</h2>
                 <form method="POST" className="register-form" id="login-form">
                   <div className="form-group pr_form-group">
+<<<<<<< Updated upstream
                     <label className='pr_label' htmlFor="your_name"><i className="fas fa-user"></i></label>
                     <input className="pa_input" type="text" name="your_name" id="your_name" placeholder="Your Name" />
                   </div>
                   <div className="form-group pr_form-group">
                     <label className='pr_label' htmlFor="your_pass"><i className="fas fa-lock"></i></label>
                     <input className="pa_input" type="password" name="your_pass" id="your_pass" placeholder="Password" />
+=======
+                    <label className="pr_label" htmlFor="your_name">
+                      <i className="fas fa-user"></i>
+                    </label>
+                    <input
+                      className="pa_input"
+                      type="text"
+                      name="username"
+                      value={state.username}
+                      onChange={handleChange}
+                      placeholder="Your Name"
+                    />
+                  </div>
+                  <div className="form-group pr_form-group">
+                    <label className="pr_label" htmlFor="your_pass">
+                      <i className="fas fa-lock"></i>
+                    </label>
+                    <input
+                      className="pa_input"
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      value={state.password}
+                      onChange={handleChange}
+                    />
+>>>>>>> Stashed changes
                   </div>
                   <div className="custom-control custom-checkbox pt-5">
                     <input type="checkbox" className="custom-control-input" id="customCheck1" />
@@ -46,7 +142,21 @@ function tenant_signin() {
                   </div>
                   <div>
                     <div className="form-group pr_form-group form-button pr_form-button">
+<<<<<<< Updated upstream
                       <input type="submit" name="signin" className=" btn btn-primary pr_form-submit" value="Sign In" />
+=======
+                      {/* <Link href="/landing/tenant"> */}
+                      <button
+                        type="submit"
+                        name="signin"
+                        className=" btn btn-primary pr_form-submit"
+                        value="Sign In"
+                        onClick={tenantSignIn}
+                      >
+                        Sign In
+                      </button>
+                      {/* </Link> */}
+>>>>>>> Stashed changes
                     </div>
                   </div>
                   <div className='p_mem'>Not a member? <a href='#'>Sign Up</a></div>
